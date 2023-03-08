689142d49390c05e5a298c0b1c6a5f6f05877cf8
OlgaVysochanska <olavysochanska@gmail.com>
2023-03-08 19:34:19 +0200

    styles add

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,15 +2,17 @@ import { Outlet } from 'react-router-dom';
 
 import { Navbar } from 'components/Navbar/Navbar';
 
+import styles from 'components/Layout/Layout.module.css';
+
 export const Layout = () => {
   return (
-    <>
+    <div className={styles.layout}>
       <header>
         <Navbar />
       </header>
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
--- src/components/Navbar/NavbarAuth/NavbarAuth.jsx
+++ src/components/Navbar/NavbarAuth/NavbarAuth.jsx
@@ -1,10 +1,16 @@
 import { NavLink } from 'react-router-dom';
 
+import styles from 'components/Navbar/NavbarAuth/NavbarAuth.module.css';
+
 export const NavbarAuth = () => {
   return (
-    <div>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/login"> | Login</NavLink>
+    <div className={styles.navbarAuth}>
+      <NavLink to="/register" className={styles.link}>
+        Register
+      </NavLink>
+      <NavLink to="/login" className={styles.link}>
+        Login
+      </NavLink>
     </div>
   );
 };
