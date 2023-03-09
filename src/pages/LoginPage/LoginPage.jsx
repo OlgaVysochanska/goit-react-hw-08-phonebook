53a89a88e84ba95e38e7749fec8dc7dee63eb720
OlgaVysochanska <olavysochanska@gmail.com>
2023-03-09 19:03:00 +0200

    edit loginpage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,26 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+
+import { login } from 'redux/auth/authOperations';
+import { isUserLogin } from 'redux/auth/authSelectors';
+
 import { LoginForm } from 'components/LoginForm/LoginForm';
 
 export const LoginPage = () => {
+  const isLogin = useSelector(isUserLogin);
+  const dispatch = useDispatch();
+
+  const handleSignup = data => {
+    console.log('handle login', data);
+    dispatch(login(data));
+  };
+
+  if (isLogin) {
+    return <Navigate to="/contacts" />;
+  }
   return (
     <div>
-      <LoginForm />
+      <LoginForm onSubmit={handleSignup} />
     </div>
   );
 };
