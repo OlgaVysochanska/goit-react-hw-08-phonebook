bb96c4288e177bd10bca834ffee3ebb7ff53ec4b
OlgaVysochanska <olavysochanska@gmail.com>
2023-03-09 19:20:06 +0200

    public route add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { Layout } from 'components/Layout/Layout';
+import { PublicRoute } from 'components/PublicRoute/PublicRoute';
 import { PrivateRoute } from 'components/PrivateRoute/PrivateRoute';
-// import { ContactForm } from './components/ContactForm/ContactForm';
-// import { Filter } from './components/Filter/Filter';
-// import { ContactList } from './components/ContactList/ContactList';
 
 import { HomePage } from 'pages/HomePage/HomePage';
 import { RegisterPage } from 'pages/RegisterPage/RegisterPage';
@@ -16,18 +14,13 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route element={<PublicRoute />}>
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
         <Route element={<PrivateRoute />}>
           <Route path="/contacts" element={<ContactsPage />} />
         </Route>
-
-        {/* <h1>Phonebook</h1>
-        <ContactForm />
-
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList /> */}
       </Route>
     </Routes>
   );
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,7 +1,14 @@
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Filter } from 'components/Filter/Filter';
+import { ContactList } from 'components/ContactList/ContactList';
+
 export const ContactsPage = () => {
   return (
     <div>
       <p>Contacts</p>
+      <ContactForm />
+      <Filter />
+      <ContactList />
     </div>
   );
 };
