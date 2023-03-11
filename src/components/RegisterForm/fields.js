de09d8547569295796db4b530970a1a1b2a9b2b7
OlgaVysochanska <olavysochanska@gmail.com>
2023-03-11 12:57:52 +0200

    edit

--- src/components/RegisterForm/fields.js
+++ src/components/RegisterForm/fields.js
@@ -3,18 +3,18 @@ export const fields = {
     type: 'text',
     name: 'name',
     required: true,
-    placeholder: 'Example: Name',
+    placeholder: 'Name',
   },
   email: {
     type: 'email',
     name: 'email',
     required: true,
-    placeholder: 'Example: email.mail.com',
+    placeholder: 'email@mail.com',
   },
   password: {
     type: 'password',
     name: 'password',
     required: true,
-    placeholder: 'Enter 6 symbols at least',
+    placeholder: '6 symbols at least',
   },
 };
